4ea2d65839652b6f5a64e3709f808e951884c4f1
faisalbaz <fbaz1982@gmail.com>
2018-10-03 11:46:38 -0700

    Index.php validated!

--- index.php
+++ index.php
@@ -2,16 +2,12 @@
    include('calcCost.php');
    
      ?>
+<!DOCTYPE HTML>
 <html lang="en">
-<head>
-	
-	<title>Your Household Expenses</title>
+   <head>
+      <title>Your Household Expenses</title>
       <link rel="stylesheet" type="text/css" href="style.css">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-   </head>
-   <style>
-      
-   </style>
    </head>
    <body>
       <div class="header">
@@ -26,37 +22,34 @@
       </div>
       <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.1/jquery.min.js'></script>
       <script  src="index.js"></script>
-
-
-
-<form action="calcCost.php" method="post" >
- <div class="input-group">
-	Taxable Quantity: <input type="number" name="productQty"  value="<?php echo $productQty; ?>" size="10" />
-	</div>
-	 <div class="input-group">
-	Price: <input type="text" name="productPrice"  value="<?php echo $productPrice; ?>" size="10" />
-	</div>
-	<div class="input-group">
-	State Tax Rate: <input type="text" name="StateTaxRate"  value="<?php echo $StateTaxRate; ?>" size="10" />
-	</div>
-	<div class="input-group">
-	Non Taxable Quantity: <input type="number" name="productQtyNT" value="<?php echo $productQtyNT; ?>" size="10" />
-	</div>
-	 <div class="input-group">
-	Price: <input type="text" name="productPriceNT"  value="<?php echo $productPriceNT; ?>"size="10" />
-	</div>
-	<div class="input-group">
-         <label>Terms and Conditions</label>
-         <textarea style="resize: none;" rows="5" cols="45" name="terms" readonly >This application is designed as part of Project 2 for CSCI E-15.</textarea>
-      </div>
-      <div class="container">
-         <section>
-            <input id="permitted" type="checkbox">
-            <label for="permitted" class="side-label">In order to continue,accept our terms and conditions.</label>
-            <button type="submit" class="input-group">Household Cost</button>
-         </section>
-      </div>
-	<input type="hidden" name="formSubmitted" value="true" />
-</form>
-</body>
+      <form action="calcCost.php" method="post" >
+         <div class="input-group">
+            Taxable Quantity: <input type="number" name="productQty"  value="<?php echo $productQty; ?>" size="10" />
+         </div>
+         <div class="input-group">
+            Price: <input type="text" name="productPrice"  value="<?php echo $productPrice; ?>" size="10" />
+         </div>
+         <div class="input-group">
+            State Tax Rate: <input type="text" name="StateTaxRate"  value="<?php echo $StateTaxRate; ?>" size="10" />
+         </div>
+         <div class="input-group">
+            Non Taxable Quantity: <input type="number" name="productQtyNT" value="<?php echo $productQtyNT; ?>" size="10" />
+         </div>
+         <div class="input-group">
+            Price: <input type="text" name="productPriceNT"  value="<?php echo $productPriceNT; ?>" size="10" />
+         </div>
+         <div class="input-group">
+            <label>Terms and Conditions</label>
+            <textarea style="resize: none;" rows="5" cols="45" name="terms" readonly >This application is designed as part of Project 2 for CSCI E-15.</textarea>
+         </div>
+         <div class="container">
+            <section>
+               <input id="permitted" type="checkbox">
+               <label for="permitted" class="side-label">In order to continue,accept our terms and conditions.</label>
+               <button type="submit" class="input-group">Household Cost</button>
+            </section>
+         </div>
+         <input type="hidden" name="formSubmitted" value="true" />
+      </form>
+   </body>
 </html>
